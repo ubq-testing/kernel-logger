92d3cffde312adad522687d08343c5e082bcc79e
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-08-29 02:38:56 +0100

    feat: update ubiquibot-logger script on postinstall

---
+++ scripts/update-logger.ts
@@ -0,0 +1,75 @@
+import { run } from "npm-check-updates";
+import fs from "fs";
+import semver from "semver";
+import { writeFile } from "fs/promises";
+import { exec } from "child_process";
+
+const LOGGER = "@ubiquity-dao/ubiquibot-logger";
+
+async function updateLogger() {
+  const packageFile = "package.json";
+  const packageJson = JSON.parse(fs.readFileSync(packageFile, "utf-8"));
+  const currentVersion = packageJson.dependencies[LOGGER] || packageJson.devDependencies[LOGGER];
+
+  if (!currentVersion) {
+    console.error(`Package ${LOGGER} is not listed in dependencies.`);
+    return;
+  }
+
+  const upgraded = await run({
+    packageFile: `${packageFile}`,
+    upgrade: false, // Don't upgrade yet, just get the latest version info for the logs
+    silent: true,
+    filter: `/${LOGGER}/`,
+  });
+
+  if (!upgraded) {
+    console.error("No updates found.");
+    return;
+  }
+
+  let latestVersion;
+
+  if (Object.keys(upgraded).length > 0 && LOGGER in upgraded) {
+    latestVersion = upgraded[LOGGER];
+  }
+
+  if (!latestVersion) {
+    console.error(`No updates found for ${LOGGER}.`);
+    return;
+  }
+
+  const codeParts = [`export const UBIQUIBOT_LOGGER_VERSION = "${latestVersion}";\n`];
+  let shouldUpdate = false;
+  if (semver.major(latestVersion) > semver.major(currentVersion)) {
+    console.warn(`Warning: Major version update available for ${LOGGER} (${currentVersion} -> ${latestVersion}).`);
+    codeParts.push(`export const HAS_NEW_MAJOR_VERSION = true;\n`);
+  } else if (semver.minor(latestVersion) > semver.minor(currentVersion) || semver.patch(latestVersion) > semver.patch(currentVersion)) {
+    console.log(`Updating ${LOGGER} from version ${currentVersion} to ${latestVersion}.`);
+    codeParts.push(`export const HAS_NEW_MAJOR_VERSION = false;\n`);
+    await run({
+      packageFile: packageFile,
+      upgrade: true,
+      silent: true,
+      filter: `/${LOGGER}/`,
+    });
+
+    shouldUpdate = true;
+  } else {
+    console.log(`No updates found for ${LOGGER}.`);
+    codeParts.push(`export const HAS_NEW_MAJOR_VERSION = false;\n`);
+  }
+
+  const code = codeParts.join("");
+  await writeFile("src/logger-version.ts", code);
+
+  if (shouldUpdate) {
+    yarnInstall();
+  }
+}
+
+async function yarnInstall() {
+  exec("yarn install");
+}
+
+updateLogger().catch(console.error);
---
+++ src/logger-version.ts
@@ -0,0 +1,2 @@
+export const UBIQUIBOT_LOGGER_VERSION = "1.3.1";
+export const HAS_NEW_MAJOR_VERSION = false;
